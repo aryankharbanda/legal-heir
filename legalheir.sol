cf7132c8ea976baca85aaf6e25f34498ab55ce5e
Aryan Kharbanda <kharbanda.aryan00@gmail.com>
2023-01-13 18:52:58 +0530

    refactor2: mapping change

--- legalheir.sol
+++ legalheir.sol
@@ -23,14 +23,12 @@ contract LegalHeir{
     }
 
     DeathCertificate[] certificates;
-
-    // !! mapping from struct to int ID
-    mapping(string => uint) certificateID;
+    mapping(string => DeathCertificate) deathCertificate;
 
     function applyDeathCert(string memory _decAID, string memory _appAID, bool _naturalDeath, string memory _firID, string memory _stationID) public {
         
         // verify if person already has deathCertificate
-        require(certificateID[_decAID] == 0, "death certificate already exists");
+        require(bytes(deathCertificate[_decAID].deceasedAID).length == 0, "death certificate already exists");
 
         DeathCertificate memory cert;
         cert.deceasedAID = _decAID;
@@ -44,67 +42,61 @@ contract LegalHeir{
         }
 
         certificates.push(cert);
-        // certificates.push(DeathCertificate(_decAID, _appAID, _fir, []));
-        
-        // minimum ID value is 1
-        certificateID[_decAID] = certificates.length;
+        deathCertificate[_decAID] = certificates[certificates.length-1];
+    }
+
+    // helper for STEP 1
+    function viewCert(string memory _decAID) public view returns (string memory applicantAID, bool isNaturalDeath, string memory firID) {
+        return(deathCertificate[_decAID].applicantAID,deathCertificate[_decAID].isNaturalDeath,deathCertificate[_decAID].firID);
     }
 
     // STEP 2 : LIST OF ELIGIBLE PEOPLE
     address officerAddress = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-    function addHeirList (string[] memory _listHeirAID, string memory _decAID) public {
+    function addHeirList(string memory _decAID, string[] memory _listHeirAID) public {
         
         // check for authorized officer
         require(msg.sender == officerAddress, "unauthorized function call");
 
         // officer should be able to add to list till legal heir not assigned
         // check if legal heir assigned already
-        require(certificates[certificateID[_decAID]-1].hasLegalHeir != true, "legal heir assigned already");
+        require(deathCertificate[_decAID].hasLegalHeir != true, "legal heir assigned already");
 
         for (uint i = 0; i < _listHeirAID.length; i++) {
-            certificates[certificateID[_decAID]-1].elibigleHeirs.push(_listHeirAID[i]);
+            deathCertificate[_decAID].elibigleHeirs.push(_listHeirAID[i]);
         }
         // !! capture relation along with aadharID
+    }
+
+    // helper for STEP 2
+    function viewEligibleList(string memory _decAID) public view returns(string[] memory listHeirAID){
+        return(deathCertificate[_decAID].elibigleHeirs);
     }
 
     // STEP 3 : APPLY LEGAL HEIR
     function applyHeir(string memory _decAID, string memory _heirAID) public {
         
         // check if death certificate exists
-        require(certificateID[_decAID] != 0, "no death certificate");
+        require(bytes(deathCertificate[_decAID].deceasedAID).length != 0, "no death certificate");
 
         // check if no one has applied before
-        require(certificates[certificateID[_decAID]-1].hasLegalHeir != true, "legal heir already assigned");
+        require(deathCertificate[_decAID].hasLegalHeir != true, "legal heir already assigned");
 
         // check if applying person is in the list
         bool isValidHeir = false;
-        for (uint i=0; i < certificates[certificateID[_decAID]-1].elibigleHeirs.length; i++) {
-            if (keccak256(abi.encodePacked(certificates[certificateID[_decAID]-1].elibigleHeirs[i])) == keccak256(abi.encodePacked(_heirAID))) {
+        for (uint i=0; i < deathCertificate[_decAID].elibigleHeirs.length; i++) {
+            if (keccak256(abi.encodePacked(deathCertificate[_decAID].elibigleHeirs[i])) == keccak256(abi.encodePacked(_heirAID))) {
                 isValidHeir = true;
             }
         }
         require(isValidHeir == true, "applicant is not eligible for legal heir");
 
         // update legalHeir in deathCertificate
-        certificates[certificateID[_decAID]-1].hasLegalHeir = true;
-        certificates[certificateID[_decAID]-1].heirAID = _heirAID;
+        deathCertificate[_decAID].hasLegalHeir = true;
+        deathCertificate[_decAID].heirAID = _heirAID;
     }
 
-    // helper functions for demo purposes
-
-    // STEP1
-    function viewCert(string memory _decAID) public view returns (string memory applicantAID, bool isNaturalDeath, string memory firID) {
-        return(certificates[certificateID[_decAID]-1].applicantAID, certificates[certificateID[_decAID]-1].isNaturalDeath, certificates[certificateID[_decAID]-1].firID);
-    }
-
-    // STEP2
-    function viewEligibleList(string memory _decAID) public view returns(string[] memory listHeirAID){
-        return(certificates[certificateID[_decAID]-1].elibigleHeirs);
-    }
-
-    // STEP3
+    // helper for STEP 3
     function viewHeir(string memory _decAID) public view returns (bool hasHeir, string memory heirAID){
-        return(certificates[certificateID[_decAID]-1].hasLegalHeir, certificates[certificateID[_decAID]-1].heirAID);
+        return(deathCertificate[_decAID].hasLegalHeir, deathCertificate[_decAID].heirAID);
     }
-
 }
